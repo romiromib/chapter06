99d2ae8dc689ccb3898fc62caefefc5062e79c1b
romiromib <raspberry.b85@gmail.com>
2024-01-17 15:27:01 +0900

    Thread

--- src/com/javaex/ex04/Multithread.java
+++ src/com/javaex/ex04/Multithread.java
@@ -4,13 +4,13 @@ public class Multithread {
 
 	public static void main(String[] args) {
 		
-		Thread thread1 = new Thread(new DigitImpl());
-		Thread thread2 = new Thread(new LowerImpl());
-		Thread thread3 = new Thread(new UpperImpl());
+		Thread t01 = new Thread(new DigitImpl());
+		Thread t02 = new Thread(new LowerImpl());
+		Thread t03 = new Thread(new UpperImpl());
 		
-		thread1.start();
-		thread2.start();
-		thread3.start();
+		t01.start();
+		t02.start();
+		t03.start();
 		
 
 	}
